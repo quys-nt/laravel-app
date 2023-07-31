07ceef968af3050eca1e4e3d343c051795f643af
nguyenquy42 <nguyenquy26012014@gmail.com>
2023-08-01 00:47:35 +0700

    up learn no.1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<h1 style="text-align: center;">Home</h1>
+
+<?php
+echo date('Y-m-d H:i:s');
+
+echo config('app.env');
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('home', function () {
+    $user = new User();
+    $allUser = $user::all();
+    dd($allUser);
+    // return view('home');
 });
