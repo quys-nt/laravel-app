dfd7390fa7d2b6fd5ccb0cbe81f7d35c92247e28
nguyenquy42 <nguyenquy26012014@gmail.com>
2023-08-10 00:38:50 +0700

    update class bootstrap

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,5 @@
-<div class="alert alert-{{ $type }} text-center">
+<div class="alert alert-{{ $type }} alert-dismissible" role="alert">
     <i class="fa fa-{{ $dataIcon }}"></i>
     {{ $content }}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
